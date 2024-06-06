ccddafbf3b46dc8b501d89fdff9c6d1f0854314a
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-06 15:56:07 +0200

    Js

--- js/main.js
+++ js/main.js
@@ -30,15 +30,33 @@ console.log(costoBiglietto);
 
 let price = parseInt(rate * distanzaKm.value);
 
-console.log("The cost of your ticket is " + price + "€");
-
 var sale20 = price - ( price * .2);
-
-console.log( "You have a 20% discount, so the cost of your ticket is: " + sale20 + "€");
 
 var sale40 = price - ( price * .4);
 
+let finale = price;
+
+console.log(finale);
+
+
+// calcolo del prezzo delle tariffe scontate
+
+console.log( "You have a 20% discount, so the cost of your ticket is: " + sale20 + "€");
+
 console.log( "You have a 40% discount, so the cost of your ticket is: " + sale40 + "€");
+
+if (etaUser == "minorenne" ) {
+    finale = "You have a 20% discount, so the cost of your ticket is: " + sale20 + " €";
+} else if(etaUser == className = ".minorenne") {
+    finale = "You have a 40% discount, so the cost of your ticket is: " + sale40 + " €";
+} else {
+    finale = "The cost of your ticket is " + price + " €";
+}
 
 // click del bottone
 
+generaTicket.addEventListener("click",
+    function () {
+
+    }
+)
